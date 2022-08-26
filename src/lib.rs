f2cccacf4348157b9fc8d8edf5f4c260b69d3cb2
AlanRandon <alan.randon@outlook.com>
2022-08-26 17:31:38 +0100

    fixed a comment that was only half there

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // Use `js_namespace` here to bind `console.log(..)` instead of just `log(..)`
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 }
